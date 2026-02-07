fd2ba6d9aa394a565effd87d1cdf40c0aa8ddcd5
Chaitra Bhat <bhatchaitra28@gmail.com>
2026-02-07 11:34:45 +0530

    Added Day1 tasks

---
+++ Day1/README.md
@@ -0,0 +1,2 @@
+![Demo Image](outputs/output1.jpeg)
+![Demo Image](outputs/output2.jpeg)
---
+++ Day1/personal_details.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+course = input("Enter your course: ")
+college = input("Enter your college name: ")
+
+print("\nPersonal Details")
+print("----------------")
+print("Name   :", name)
+print("Age    :", age)
+print("Course :", course)
+print("College:", college)
