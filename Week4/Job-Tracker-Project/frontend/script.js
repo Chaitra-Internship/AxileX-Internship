81eb071fc065c85797443dd5022b1b1dd0291ebc
Chaitra-Internship <cbhat940@gmail.com>
2026-03-05 23:05:42 +0530

    Connected frontend with backend API

--- Week4/Job-Tracker-Project/frontend/script.js
+++ Week4/Job-Tracker-Project/frontend/script.js
@@ -1,4 +1,4 @@
-fetch("https://your-render-url.onrender.com/api")
+fetch("https://axilex-internship-1.onrender.com/api")
 .then(res => res.json())
 .then(data => {
 document.getElementById("output").innerText = data.message
